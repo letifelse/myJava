b25f12ba9604d1cf3e7b375bd689152a8cc9f995
Artem Fedianin <let.if.else@gmail.com>
2022-06-29 15:43:46 +0400

    add new settings

--- src/HellRus.java
+++ src/HellRus.java
@@ -1,6 +1,6 @@
 public class HellRus {
     public static void main(String[] args) {
-        String s = "Привет, Hello";
+        String s = "Привет, аллое, Hello";
         System.out.println(s);
         // комментарий привет.
     }
