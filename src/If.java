b5d4aae0eac8fb0bd1cbf58eb0d08d2a401c13ee
Artem Fedianin <let.if.else@gmail.com>
2022-06-29 16:12:40 +0400

    ignore

--- src/If.java
+++ src/If.java
@@ -1,9 +1,9 @@
 public class If {
     public static void main(String[] args) {
         int myInt = 15;
-        if (myInt < 10) {
+        if (myInt > 10) {
             System.out.println("Hey, dude");
-        } else if (myInt > 20) {
+        } else if (myInt < 20) {
             System.out.println("NO, dude");
         } else {
             System.out.println("Whatever");
