676122a7ee0769ad8db08a5c56cfd05787b694d2
Artem Fedianin <let.if.else@gmail.com>
2022-06-29 16:41:49 +0400

    new settings 2

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,6 +1,6 @@
 public class ForLoops {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i = i - 5) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println("YO! " + i); // i++; = i + 1;
         }
 
